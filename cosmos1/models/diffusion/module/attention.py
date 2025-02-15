6502373e565c23a775c423567f047c42b4fefdc1
saloniparekh <saloni.parekh@wayve.ai>
2025-02-15 01:47:05 +0000

    Union import

--- cosmos1/models/diffusion/module/attention.py
+++ cosmos1/models/diffusion/module/attention.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import numpy as np
 import torch
